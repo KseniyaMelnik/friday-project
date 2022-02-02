97c2db4eb3c7ee0d585eccc7681f515a6553d8b2
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-02-02 17:03:12 +0300

    feat: add provider to progect

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 ReactDOM.render(
-  <HashRouter>
-          <App />
-  </HashRouter>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
