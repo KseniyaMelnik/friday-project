a51e753b4671511f163b132890c2123b0c1c4cb5
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-02-04 17:57:22 +0300

    feat: add reducers to store

---
+++ src/store/retrieval-reducer.ts
@@ -0,0 +1,15 @@
+import { Dispatch } from 'redux'
+
+
+const initialState = {
+
+}
+type InitialStateType = typeof initialState
+
+type ActionsType = {
+
+}
+
+export const retrievalReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    return state
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,19 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
+import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {appReducer} from "./app-reducer";
+import {loginReducer} from "./login-reducer";
+import {registrationReducer} from "./registration-reducer";
+import {newPasswordReducer} from "./new-password-reducer";
+import {profileReducer} from "./profile-reducer";
+import {retrievalReducer} from "./retrieval-reducer";
 
 
 const rootReducer = combineReducers({
- app: appReducer
+    login: loginReducer,
+    registration: registrationReducer,
+    newPassword: newPasswordReducer,
+    profile: profileReducer,
+    retrieval: retrievalReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
